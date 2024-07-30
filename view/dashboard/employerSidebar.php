f0ea37c6fbfb290cff5258a4006125a4c881d1da
Faustino25 <127408104+Voided-Name@users.noreply.github.com>
2024-07-30 09:52:21 +0800

    loader fixed and changed

--- view/dashboard/employerSidebar.php
+++ view/dashboard/employerSidebar.php
@@ -146,3 +146,24 @@ if ($_SESSION['employerPage'] == "createNewJob") {
   </li>
 -->
           </ul>
+          <script>
+            if (sessionStorage.getItem('sidebarState') == 'mini') {
+              const sideBar = document.getElementById('sidebar');
+
+              sideBar.classList.add("sidebar-mini");
+            }
+
+            document.addEventListener('DOMContentLoaded', function() {
+              const sidebarToggle = document.querySelector('.sidebar-toggle');
+              const sideBar = document.getElementById('sidebar');
+
+
+              sidebarToggle.addEventListener('click', function() {
+                if (sideBar.classList.contains('sidebar-mini')) {
+                  sessionStorage.setItem('sidebarState', 'mini');
+                } else {
+                  sessionStorage.setItem('sidebarState', 'default');
+                }
+              });
+            });
+          </script>
--- view/dashboard/facultySidebar.php
+++ view/dashboard/facultySidebar.php
@@ -175,3 +175,24 @@ if ($_SESSION['facultyPage'] == "manageProfile") {
                 </li>
               </ul>
             </li>
+            <script>
+              if (sessionStorage.getItem('sidebarState') == 'mini') {
+                const sideBar = document.getElementById('sidebar');
+
+                sideBar.classList.add("sidebar-mini");
+              }
+
+              document.addEventListener('DOMContentLoaded', function() {
+                const sidebarToggle = document.querySelector('.sidebar-toggle');
+                const sideBar = document.getElementById('sidebar');
+
+
+                sidebarToggle.addEventListener('click', function() {
+                  if (sideBar.classList.contains('sidebar-mini')) {
+                    sessionStorage.setItem('sidebarState', 'mini');
+                  } else {
+                    sessionStorage.setItem('sidebarState', 'default');
+                  }
+                });
+              });
+            </script>
